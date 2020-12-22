80132d98bea92426f812fcbaaabd0431426fa0a1
David Tavarez <david@punksec.com>
2020-12-22 11:19:10 +0100

    adding extra headers

--- check/index.php
+++ check/index.php
@@ -25,6 +25,8 @@ curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Cache-Control: no-cache',
+    'Pragma: no-cache',
     'Content-Type: application/json',
     'Content-Length: ' . strlen($params)
 ));
@@ -36,6 +38,9 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_PROXY, "127.0.0.1:9050");
 curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5_HOSTNAME);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+curl_setopt($ch, CURLOPT_TCP_NODELAY, false);
+curl_setopt($ch, CURLOPT_TCP_FASTOPEN, true);
 
 $output = curl_exec($ch);
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
